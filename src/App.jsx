75045f7b9ec0ec4426654ecaaa5575405eb3a812
TheJobGG <jobglez80@gmail.com>
2023-01-20 02:05:36 -0700

    Add color symbology for "in stock" and "out of stock" products

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ function App() {
     <div className="px-10 pb-10">
       <ProductForm />
       <hr className='max-w-[975px] mx-auto' />
-      <h2 className='text-center text-2xl py-5'>Added Products</h2>
+      <div className='flex flex-col items-center justify-center gap-x-5 gap-y-5 sm:flex-row lg:ml-10'>
+        <h2 className='text-center text-2xl py-5'>Added Products</h2>
+        <div className='flex gap-x-2 items-center sm-max:-translate-y-5'>
+          <span className='flex w-5 h-5 bg-slate-700 border'></span>
+          <span>In stock</span></div>
+        <div className='flex gap-x-2 items-center sm-max:-translate-y-5 '>
+          <span className='flex w-5 h-5 bg-[#2E0219] border'></span>
+          <span>Out of stock</span></div>
+      </div>
 
       <div className='flex flex-wrap gap-10 justify-center'>
         <Products />
