c4c9e9dfcdf1813b416f754c94259c6cf6e6273c
TheJobGG <jobglez80@gmail.com>
2023-01-20 02:29:57 -0700

    Manage black spaces and uppercase first letter

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -21,16 +21,20 @@ function ProductForm() {
     const product = Object.fromEntries(formData);
 
     const tags = splitTags(product.tags)
-    product.tags = tags
-    console.log(tags);
 
-    addProductMutation.mutate({
-      ...product, inStock: true
-    })
+    product.tags = tags
+    product.category = uppercaseFirstLetter(product.category) ?? 'Sin categoría'
+
+    if (product.stock < 0) product.stock = 0
+
+    addProductMutation.mutate(product)
     // e.target.reset();
   }
 
-  const splitTags = (tags) => tags.split(',').map(tag => tag.trim()).filter(tag => tag != "")
+  const splitTags = (tags) => tags.split(',').map(tag => tag.trim()).filter(tag => tag != "").map(tag => uppercaseFirstLetter(tag))
+  const uppercaseFirstLetter = (phrase) => phrase.charAt(0).toUpperCase() + phrase.slice(1)
+
+  const handleBlankSpaces = (e) => e.currentTarget.value.trim() ? e.currentTarget.value : e.currentTarget.value = e.currentTarget.value.trim()
 
   return (
     <div className="py-10 px-5">
@@ -43,24 +47,34 @@ function ProductForm() {
           {/* Input: Name */}
           <div className='flex flex-col'>
             <label htmlFor="name">Name</label>
-            <input placeholder='Headphones' className='rounded-md text-black px-3 py-1' required type="text" name="name" id="name" />
+            <input
+              required
+              placeholder='Headphones'
+              className='rounded-md text-black px-3 py-1'
+              type="text"
+              name="name"
+              id="name"
+              onChange={(e) => handleBlankSpaces(e)}
+            />
           </div>
 
 
           {/* Price and Stock */}
           <div className='flex gap-5 lg-max:flex-col'>
-            
+
             {/* Input: Price */}
             <div className='flex flex-col relative'>
               <label htmlFor="price">Price</label>
               <span className=' text-black absolute bottom-1 left-3'>$ </span>
               <input
                 required
+                min={0}
                 className='rounded-md text-black px-6 py-1 '
                 placeholder='200'
                 type="number"
                 name="price"
                 id="price"
+                onChange={(e) => handleBlankSpaces(e)}
               />
             </div>
 
@@ -74,6 +88,7 @@ function ProductForm() {
                 type="number"
                 name="stock"
                 id="stock"
+                onChange={(e) => handleBlankSpaces(e)}
               />
             </div>
           </div>
@@ -82,7 +97,15 @@ function ProductForm() {
           {/* Input: Tags */}
           <div className='flex flex-col'>
             <label htmlFor="tags">Tags</label>
-            <input placeholder='Divide, Tags, By Commas' className='rounded-md text-black px-3 py-1' required type="text" name="tags" id="tags" />
+            <input
+              placeholder='Divide, Tags, By Commas'
+              className='rounded-md text-black px-3 py-1'
+              required
+              type="text"
+              name="tags"
+              id="tags"
+              onChange={(e) => handleBlankSpaces(e)}
+            />
           </div>
           <button className="bg-teal-600 border border-white py-1 rounded-md lg-max:hidden hover:opacity-75 transition-opacity" type='submit'>Add product</button>
         </div>
@@ -91,12 +114,29 @@ function ProductForm() {
           {/* Input: Category */}
           <div className='flex flex-col'>
             <label htmlFor="category">Category</label>
-            <input placeholder='Electronics' className='rounded-md text-black px-3 py-1' required type="text" name="category" id="category" />
+            <input
+              required
+              placeholder='Electronics'
+              className='rounded-md text-black px-3 py-1'
+              type="text"
+              name="category"
+              id="category"
+              onChange={(e) => handleBlankSpaces(e)}
+            />
           </div>
           {/* Input: Description */}
           <div className='flex flex-col'>
             <label htmlFor="description">Description</label>
-            <textarea placeholder={`Headphones are a pair of small loudspeaker drivers worn on or around the head over a user's ears.\n\n - Wikipedia`} className='rounded-md text-black px-3 py-1' required cols={40} rows={5} name="description" id="description" />
+            <textarea
+              required
+              placeholder={`Headphones are a pair of small loudspeaker drivers worn on or around the head over a user's ears.\n\n - Wikipedia`}
+              className='rounded-md text-black px-3 py-1'
+              cols={40}
+              rows={5}
+              name="description"
+              id="description"
+              onChange={(e) => handleBlankSpaces(e)}
+            />
             <span className='my-5 text-left opacity-30 md:text-center'>Tip: Make sure you add a good description</span>
             <button className="bg-teal-900 border border-white py-1 rounded-md md:hidden" type='submit'>Add product</button>
           </div>
